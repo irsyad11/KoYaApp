fe0fca7c0461ada357778ad9d5eee4d34ceb1194
irsyad11 <47568499+irsyad11@users.noreply.github.com>
2022-03-29 19:24:19 +0800

    new format time stamp

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -3,6 +3,12 @@ import sequelize from "sequelize";
 const db = new sequelize("koya_db", "root", "", {
   host: "localhost",
   dialect: "mysql",
+  dialectOptions: {
+    useUTC: false, //for reading from database
+    dateStrings: true,
+    typeCast: true,
+  },
+  timezone: "+08:00", //for writing to database
 });
 
 export default db;
